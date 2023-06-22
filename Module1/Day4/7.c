dcfb1280804ddc9fdb1a8e4d6bf5d0f83f80a165
Shafaiz <shafaizahmedkhan27@gmail.com>
2023-06-22 22:09:02 +0530

    commit 4 : main : Day4 Activities added

---
+++ Module1/Day4/7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[100];
+    
+    printf("Enter a string: ");
+    scanf("%s", str);
+    
+    int num = atoi(str);
+    
+    printf("Converted integer: %d\n", num);
+    
+    return 0;
+}
+
